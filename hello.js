43131b4396cddbc9ff804d42d3142bc76e3eb99f
Danny <dannyglv182@gmail.com>
2022-05-23 20:01:30 -0700

    Added contact section icon and removed placeholder photos.

--- hello.js
+++ hello.js
@@ -1,14 +1,20 @@
 var projectOneDesc = document.getElementById('description_1');
 var projectTwoDesc = document.getElementById('description_2');
+var projectThreeDesc = document.getElementById('description_3');
 var text1 = document.createTextNode(`A Flask application where users can log in and share posts.
                                     The passlib libary is used to hash and verify passwords. API endpoints exist for
                                     adding users and blog posts. JSON and jQuery make posts appear asynchronously.
                                     `);
 
 
-var text2 = document.createTextNode(`A Connect Four game played against the computer. Pytest is used for testing and moves
-                                     are played as row and column combinations. 
+var text2 = document.createTextNode(`A Connect Four game written in Python. Moves are played as row and column combinations and each match is played
+                                     against played against the computer. Pytest is used for testing.
+                                    `);
+
+var text3 = document.createTextNode(`A PostgresSQL database built from a CSV file. The database contains entries for films and their associated data.
+                                     Psycopg2 and Pandas are used for the ETL process. 
                                     `);
 
 projectOneDesc.appendChild(text1);
 projectTwoDesc.appendChild(text2);
+projectThreeDesc.appendChild(text3);
